72413ffd3ec91bfcb3630b2471a52e85779bbbc0
vitrum <vitrum.cn@gmail.com>
2020-05-06 16:28:04 +0800

    添加样式

--- src/App.vue
+++ src/App.vue
@@ -9,21 +9,28 @@
 </template>
 
 <style>
+body {
+  color: #42b983;
+  text-align: center;
+  background-color: #797a7a;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #3c3c3c;
+  -webkit-box-flex: 1;
+    flex: 1 1 100%;
 }
 
 #nav {
-  padding: 30px;
+  padding: 10px 30px;
 }
 
 #nav a {
   font-weight: bold;
-  color: #2c3e50;
+  color: #54b881;
 }
 
 #nav a.router-link-exact-active {
