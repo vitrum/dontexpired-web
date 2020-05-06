42de5375382eaa833d39bbc62b73b8b484b693f6
vitrum <vitrum.cn@gmail.com>
2020-05-06 15:54:09 +0800

    调整标题

--- vue.config.js
+++ vue.config.js
@@ -3,4 +3,17 @@ const BASE_URL = process.env.NODE_ENV === 'production' ? '/dontexpired-web/' : '
 module.exports = {
   publicPath: BASE_URL,
   outputDir: 'docs',
+  pages: {
+    index: {
+      // page 的入口
+      entry: 'src/main.js',
+      // 模板来源
+      template: 'public/index.html',
+      // 在 dist/index.html 的输出
+      filename: 'index.html',
+      // 当使用 title 选项时，
+      // template 中的 title 标签需要是 <title><%= htmlWebpackPlugin.options.title %></title>
+      title: '别过期了！'
+    }
+  }
 }
