993b9efa46fa99b39a9c7f227272b7d5957bb394
vitrum <vitrum.cn@gmail.com>
2020-05-20 21:36:48 +0800

    调整cloud 函数

--- cloud.js
+++ cloud.js
@@ -1,22 +1,21 @@
-const AV = require("leancloud-storage");
+const AV = require('leanengine')
+const fs = require('fs')
+const path = require('path')
 
+/**
+ * 加载 functions 目录下所有的云函数
+ */
+fs.readdirSync(path.join(__dirname, 'functions')).forEach( file => {
+  require(path.join(__dirname, 'functions', file))
+})
 
+/**
+ * 一个简单的云代码方法
+ */
+AV.Cloud.define('hello', function(request) {
+  return 'Hello world!'
+})
 
-
-console.log(AV.Cloud)
-
-AV.Cloud.define("checkStatus", () => {
-  const now = new Date();
-  const tomorrow = new Date();
-  tomorrow.setDate(now.getDate() + 1);
-  const todoQuery = new AV.Query("TestObject");
-  todoQuery
-    // .greaterThan("due", now)
-    // .lessThan("due", tomorrow)
-    // .equalTo("completed", false)
-    // .ascending("due")
-    .find()
-    .then(todos => {
-      console.log('todos', todos)
-    });
-});
+AV.Cloud.define('look', function(request) {
+  return 'look down!'
+})
